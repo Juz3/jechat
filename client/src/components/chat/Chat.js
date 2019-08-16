5254c40f0e4f7326384dda38ebfb963880c62532
Koivumäki Jussi <koivujus@gmail.com>
2019-08-16 17:06:51 +0300

    added object structure to messages, a bit broken

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -8,8 +8,9 @@ class Chat extends React.Component {
     super(props);
     this.state = {
       conversation: [],
-      newMessage: "",
-      timestamp: []
+      newMessage: {},
+      timestamp: [],
+      loaded: false
     };
   }
 
@@ -17,13 +18,13 @@ class Chat extends React.Component {
     socket.on("send message", payload => {
       console.log(payload);
       this.setState({
-        conversation: payload.msg,
-        timestamp: payload.timestamp
+        conversation: payload,
+        loaded: true
       });
     });
   }
 
-  sendMessage = () => {
+  /*   sendMessage = () => {
     const newMsg = this.state.newMessage;
     const msg = this.state.conversation;
 
@@ -35,38 +36,66 @@ class Chat extends React.Component {
     });
     socket.emit("send message", this.state.conversation);
     console.log("send message");
+  }; */
+
+  sendMessage = () => {
+    const newMessage = this.state.newMessage;
+    const convo = this.state.conversation;
+
+    convo.push(newMessage);
+    console.log(convo);
+
+    this.setState({
+      conversation: convo
+    });
+    socket.emit("send message", this.state.conversation);
+    console.log("send message");
   };
 
-  // Get time from server instead of this
   getTimeStamp = () => {
-    const time = this.state.timestamp;
-    return time;
+    const time = new Date();
+    const hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    if (seconds < 10) seconds = ("0" + seconds).toString();
+    if (minutes < 10) minutes = ("0" + minutes).toString();
+
+    const formattedTime = hours + ":" + minutes + ":" + seconds;
+    console.log(formattedTime.toString());
+    return formattedTime.toString();
   };
 
   render() {
+    const conversation = this.state.loaded
+      ? this.state.conversation.map((entry, index) => (
+          <li key={index}>
+            <div className="username">{entry.user}</div>
+            <div className="timestamp">{entry.timestamp}</div>
+            <div className="msg">{entry.msg}</div>
+          </li>
+        ))
+      : null;
+
+    console.log(this.state.conversation);
+
     return (
       <Fragment>
-        <ul className="messages">
-          {this.state.conversation.map((msg, index) => (
-            <li key={index}>
-              <div className="username">username</div>
-              <div className="timestamp">{this.getTimeStamp()}</div>
-              <div className="msg">{msg}</div>
-            </li>
-          ))}
-        </ul>
+        <ul className="messages">{conversation}</ul>
 
         <form
           className="messageForm"
           onSubmit={e => {
             e.preventDefault();
-            this.state.newMessage.length > 0 ? this.sendMessage() : console.log("msg empty");
+            this.state.newMessage.msg.length > 0 ? this.sendMessage() : console.log("msg empty");
           }}
         >
           <input
             className="textInput"
             onBlur={e => {
-              this.setState({ newMessage: e.target.value });
+              this.setState({
+                newMessage: { user: "newUser", msg: e.target.value, timestamp: this.getTimeStamp() }
+              });
               e.target.value = "";
             }}
           />
--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer);
 
 let msgMemory = [];
-let tsMemory = [];
 
 io.on("connection", socket => {
   console.log(`User # connected!`);
 
+  io.emit("send message", [
+    {
+      user: "user1",
+      msg: "msg1",
+      timestamp: "9:99:99PM"
+    }
+  ]);
+
   socket.on("send message", msg => {
     console.log("new message: ", msg);
 
-    // Save to memory, no db yet
+    /*     // Save to memory, no db yet
     msgMemory.push(msg[msg.length - 1]);
 
-    console.log("mem: ", msgMemory);
+    console.log("mem: ", msgMemory); */
 
     const timestamp = new Date();
     const formattedTime = (
@@ -26,12 +33,13 @@ io.on("connection", socket => {
       timestamp.getSeconds()
     ).toString();
 
-    tsMemory.push(formattedTime);
-
-    payload = {
-      msg: msg,
-      timestamp: formattedTime
-    };
+    payload = [
+      {
+        user: "defaultUser",
+        msg: msg,
+        timestamp: timestamp
+      }
+    ];
     // emit to clients
     io.emit("send message", payload);
     console.log(formattedTime);
